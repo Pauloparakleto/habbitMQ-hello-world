e2c200f3f61b4f16a04cc8210f340042e63607e2
paulo-felipe <alparakleto@gmail.com>
2022-07-09 14:16:50 -0300

    new task.rb the sender listening bash inputs

---
+++ new_task.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+require 'bunny'
+
+connection = Bunny.new
+connection.start
+
+channel = connection.create_channel
+queue = channel.queue('hello')
+
+message = ARGV.empty? ? 'Hello World!' : ARGV.join('')
+
+queue.publish(message, persistent: true)
+puts "[x] Sent #{message}"
+
+#channel.default_exchange.publish('Hello, is it me are you looking for?', routing_key: queue.name)
+#puts " [x] Sent 'Hello World!'"
+
+connection.close
